d21a37218b71cff65496bc5033921826492a9e8b
AlejandroC007 <yeineralejandro3@gmail.com>
2023-06-29 18:36:28 -0500

    Se añadieron nuevos documentos al repositorio

---
+++ Jesús/Propuesta/JS/script.js
@@ -0,0 +1,5 @@
+/* Var sirve para definir una variable, y el prompt para capturar información */
+var firtsname=prompt("Por favor escriba su nombre");
+
+/* El + es para concatenar/unir dos funciones o "invocar" una. En este caso el nombre que se introduce en la variable firtsname */ 
+alert("Bienvenido, "+firtsname+" a la presentación de la propuesta")
